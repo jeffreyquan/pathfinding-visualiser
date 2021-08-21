803f809288fdecd105074adbee9704dcab26221d
Jeffrey <jeffrey.quan@hotmail.com>
2021-08-21 22:10:33 +1000

    allow diagonals option for dijkstra

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -2,7 +2,7 @@
 // in which they were visited. Also make nodes point back to their
 // previous node, effectively allowing us to compute the shortest path
 // by backtracking from the finish node.
-export function dijkstra(grid, startNode, finishNode) {
+export function dijkstra(grid, startNode, finishNode, allowDiagonals = false) {
   const visitedNodesInOrder = [];
   startNode.distance = 0;
   const unvisitedNodes = getAllNodes(grid);
@@ -17,7 +17,7 @@ export function dijkstra(grid, startNode, finishNode) {
     closestNode.isVisited = true;
     visitedNodesInOrder.push(closestNode);
     if (closestNode === finishNode) return visitedNodesInOrder;
-    updateUnvisitedNeighbors(closestNode, grid);
+    updateUnvisitedNeighbors(closestNode, grid, allowDiagonals);
   }
 }
 
@@ -25,21 +25,36 @@ function sortNodesByDistance(unvisitedNodes) {
   unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
 
-function updateUnvisitedNeighbors(node, grid) {
-  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+function updateUnvisitedNeighbors(node, grid, allowDiagonals) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid, allowDiagonals);
   for (const neighbor of unvisitedNeighbors) {
     neighbor.distance = node.distance + 1;
     neighbor.previousNode = node;
   }
 }
 
-function getUnvisitedNeighbors(node, grid) {
+function getUnvisitedNeighbors(node, grid, allowDiagonals) {
   const neighbors = [];
   const { col, row } = node;
   if (row > 0) neighbors.push(grid[row - 1][col]);
   if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
   if (col > 0) neighbors.push(grid[row][col - 1]);
   if (col < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+
+  if (allowDiagonals) {
+    // Diagonals
+    // Top-left
+    if (row > 0 && col > 0) neighbors.push(grid[row - 1][col - 1]);
+    // Bottom-left
+    if (row < grid.length - 1 && col > 0)
+      neighbors.push(grid[row + 1][col - 1]);
+    // Top-right
+    if (row > 0 && col < grid[0].length - 1)
+      neighbors.push(grid[row - 1][col + 1]);
+    // Bottom-right
+    if (row < grid.length - 1 && col < grid[0].length - 1)
+      neighbors.push(grid[row + 1][col + 1]);
+  }
   return neighbors.filter((neighbor) => !neighbor.isVisited);
 }
 
