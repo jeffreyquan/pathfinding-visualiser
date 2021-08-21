b26cc055bd38e6a3525e6e2845147907dd228dbc
Jeffrey <jeffrey.quan@hotmail.com>
2021-08-21 22:03:07 +1000

    ♻️  moved logic into separate helpers file

--- src/PathfindingVisualiser/PathfindingVisualiser.jsx
+++ src/PathfindingVisualiser/PathfindingVisualiser.jsx
@@ -11,18 +11,37 @@ import { getNodesInShortestPathOrder } from "../algorithms/utils";
 import Node from "./Node";
 import styles from "./PathfindingVisualiser.module.css";
 import nodeStyles from "./Node/Node.module.css";
+import {
+  createGrid,
+  getNewGridWithWallToggled,
+  resetAlgorithm,
+  setInitialNode,
+  toggleFinishNode,
+  toggleStartNode,
+} from "./helpers";
 
+// Initial Grid Constants
 const NO_OF_ROWS = 21;
-const NO_OF_COLS = 41;
-const INITIAL_START_NODE_ROW = 10;
-const INITIAL_START_NODE_COL = 5;
-const INITIAL_FINISH_NODE_ROW = 10;
-const INITIAL_FINISH_NODE_COL = 35;
+const NO_OF_COLS = 21;
+const START_NODE = {
+  row: 10,
+  col: 5,
+};
+const FINISH_NODE = {
+  row: 10,
+  col: 15,
+};
 
 export default function PathfindingVisualiser() {
-  const [grid, setGrid] = useState(() => setInitialGrid());
-  const [startNode, setStartNode] = useState(() => setInitialStartNode());
-  const [finishNode, setFinishNode] = useState(() => setInitialFinishNode());
+  const [grid, setGrid] = useState(() =>
+    createGrid(START_NODE, FINISH_NODE, NO_OF_ROWS, NO_OF_COLS)
+  );
+
+  const [startNode, setStartNode] = useState(() => setInitialNode(START_NODE));
+
+  const [finishNode, setFinishNode] = useState(() =>
+    setInitialNode(FINISH_NODE)
+  );
 
   const [nodesInShortestPathOrder, setNodesInShortestPathOrder] = useState([]);
   const [visitedNodesInOrder, setVisitedNodesInOrder] = useState([]);
@@ -70,27 +89,13 @@ export default function PathfindingVisualiser() {
   const [disabledGrid, setDisabledGrid] = useState(false);
 
   const [mouseIsPressed, setMouseIsPressed] = useState(false);
+
   const [allowDiagonals, setAllowDiagonals] = useState(false);
 
-  function visualiseDijkstra() {
+  function visualiseAlgorithm(algorithm) {
     setDisabledGrid(true);
     setVisitedNodesInOrder(
-      dijkstra(
-        grid,
-        grid[startNode.row][startNode.col],
-        grid[finishNode.row][finishNode.col]
-      )
-    );
-    setNodesInShortestPathOrder(
-      getNodesInShortestPathOrder(grid[finishNode.row][finishNode.col])
-    );
-  }
-
-  function visualiseAStar() {
-    setDisabledGrid(true);
-    console.log({ nodesInShortestPathOrder, visitedNodesInOrder });
-    setVisitedNodesInOrder(
-      astar(
+      algorithm(
         grid,
         grid[startNode.row][startNode.col],
         grid[finishNode.row][finishNode.col],
@@ -112,6 +117,7 @@ export default function PathfindingVisualiser() {
     return row === finishNode.row && col === finishNode.col;
   }
 
+  // Mouse events on nodes
   function handleMouseDown(row, col) {
     if (disabledGrid) return;
 
@@ -138,7 +144,6 @@ export default function PathfindingVisualiser() {
 
     if (!finishNode && !isStartNode(row, col)) {
       toggleFinishNode(grid, row, col, true);
-      console.log("setting finish node");
       setFinishNode(() => ({
         row,
         col,
@@ -191,16 +196,22 @@ export default function PathfindingVisualiser() {
   function reset() {
     resetShortestPath();
     resetVisitedNodes();
-    setGrid(setInitialGrid());
-    setStartNode(setInitialStartNode());
-    setFinishNode(setInitialFinishNode());
+    resetToInitialGrid();
     setDisabledGrid(false);
+  }
+
+  function resetToInitialGrid() {
+    setGrid(createGrid(START_NODE, FINISH_NODE, NO_OF_ROWS, NO_OF_COLS));
+    setStartNode(setInitialNode(START_NODE));
+    setFinishNode(setInitialNode(FINISH_NODE));
   }
 
   return (
     <>
-      <button onClick={() => visualiseAStar()}>Visualise A* Algorithm</button>
-      <button onClick={() => visualiseDijkstra()}>
+      <button onClick={() => visualiseAlgorithm(astar)}>
+        Visualise A* Algorithm
+      </button>
+      <button onClick={() => visualiseAlgorithm(dijkstra)}>
         Visualise Dijkstra's Algorithm
       </button>
       <button onClick={() => resetPath()}>Reset Grid</button>
@@ -235,116 +246,5 @@ export default function PathfindingVisualiser() {
         ))}
       </div>
     </>
-  );
-}
-
-function createNode(row, col, isStart = false, isFinish = false) {
-  return {
-    col,
-    row,
-    isStart,
-    isFinish,
-    distance: Infinity,
-    isVisited: false,
-    isWall: false,
-    previousNode: null,
-    fScore: Infinity,
-    gScore: Infinity,
-  };
-}
-
-function toggleFinishNode(grid, row, col, isFinish) {
-  const newGrid = grid.slice();
-  const node = newGrid[row][col];
-  const newNode = {
-    ...node,
-    isFinish,
-  };
-  newGrid[row][col] = newNode;
-  return newGrid;
-}
-
-function toggleStartNode(grid, row, col, isStart) {
-  const newGrid = grid.slice();
-  const node = newGrid[row][col];
-  const newNode = {
-    ...node,
-    isStart,
-  };
-  newGrid[row][col] = newNode;
-  return newGrid;
-}
-
-function getNewGridWithWallToggled(grid, row, col) {
-  const newGrid = grid.slice();
-  const node = newGrid[row][col];
-  const newNode = {
-    ...node,
-    isWall: !node.isWall,
-  };
-  newGrid[row][col] = newNode;
-  return newGrid;
-}
-
-function createGrid(startNodeRow, startNodeCol, finishNodeRow, finishNodeCol) {
-  const grid = [];
-
-  for (let row = 0; row < NO_OF_ROWS; row++) {
-    const currentRow = [];
-    for (let col = 0; col < NO_OF_COLS; col++) {
-      if (row === startNodeRow && col === startNodeCol) {
-        currentRow.push(createNode(row, col, true, false));
-        continue;
-      }
-
-      if (row === finishNodeRow && col === finishNodeCol) {
-        currentRow.push(createNode(row, col, false, true));
-        continue;
-      }
-
-      currentRow.push(createNode(row, col));
-    }
-    grid.push(currentRow);
-  }
-
-  return grid;
-}
-
-function setInitialGrid() {
-  return createGrid(
-    INITIAL_START_NODE_ROW,
-    INITIAL_START_NODE_COL,
-    INITIAL_FINISH_NODE_ROW,
-    INITIAL_FINISH_NODE_COL
-  );
-}
-
-function setInitialStartNode() {
-  return {
-    row: INITIAL_START_NODE_ROW,
-    col: INITIAL_START_NODE_COL,
-  };
-}
-
-function setInitialFinishNode() {
-  return {
-    row: INITIAL_FINISH_NODE_ROW,
-    col: INITIAL_FINISH_NODE_COL,
-  };
-}
-
-function resetAlgorithm(grid, startNode, finishNode) {
-  return grid.map((row, rowIndex) =>
-    row.map((node, colIndex) => {
-      if (node.isWall) {
-        const newNode = createNode(rowIndex, colIndex);
-        newNode.isWall = true;
-        return newNode;
-      }
-      const isStart = rowIndex === startNode.row && colIndex === startNode.col;
-      const isFinish =
-        rowIndex === finishNode.row && colIndex === finishNode.col;
-      return createNode(rowIndex, colIndex, isStart, isFinish);
-    })
   );
 }
